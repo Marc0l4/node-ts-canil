5494780595671d03d29257a2bb59c17f79af38c0
Marco Francisco de Oliveira Junior <mj991982@outlook.com>
2024-06-13 16:33:51 -0300

    edit pages to mustache

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -2,25 +2,42 @@ import { Request, Response } from "express";
 
 export const home = async (req: Request, res: Response) => {
 
-    res.render('pages/page');
+    res.render('pages/page', {
+        banner: {
+            title: 'Todos os animais',
+            background: 'allanimals.jpg'
+        }
+    });
 };
 
 export const dogs = async (req: Request, res: Response) => {
 
-
-    res.render('pages/page');
+    res.render('pages/page', {
+        banner: {
+            title: 'Cachorros',
+            background: 'banner_dog.jpg'
+        }
+    });
 };
 
 export const cats = async (req: Request, res: Response) => {
 
-
-    res.render('pages/page');
+    res.render('pages/page', {
+        banner: {
+            title: 'Gatos',
+            background: 'banner_cat.jpg'
+        }
+    });
 };
 
 export const fishes = async (req: Request, res: Response) => {
 
-
-    res.render('pages/fishes');
+    res.render('pages/page', {
+        banner: {
+            title: 'Peixes',
+            background: 'banner_fish.jpg'
+        }
+    });
 };
 
 export const notFound = async (req: Request, res: Response) => {
