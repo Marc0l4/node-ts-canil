30cccc9d316dac2039debb34fad1ef788ddcd1f1
Marco Francisco de Oliveira Junior <mj991982@outlook.com>
2024-06-13 17:45:38 -0300

    edit header

--- src/controllers/PageController.ts
+++ src/controllers/PageController.ts
@@ -1,8 +1,11 @@
 import { Request, Response } from "express";
 
+import { createMenuObject } from "../helpers/createMenuObject";
+
 export const home = async (req: Request, res: Response) => {
 
     res.render('pages/page', {
+        menu: createMenuObject('all'),
         banner: {
             title: 'Todos os animais',
             background: 'allanimals.jpg'
@@ -13,6 +16,7 @@ export const home = async (req: Request, res: Response) => {
 export const dogs = async (req: Request, res: Response) => {
 
     res.render('pages/page', {
+        menu: createMenuObject('dog'),
         banner: {
             title: 'Cachorros',
             background: 'banner_dog.jpg'
@@ -23,6 +27,7 @@ export const dogs = async (req: Request, res: Response) => {
 export const cats = async (req: Request, res: Response) => {
 
     res.render('pages/page', {
+        menu: createMenuObject('cat'),
         banner: {
             title: 'Gatos',
             background: 'banner_cat.jpg'
@@ -33,6 +38,7 @@ export const cats = async (req: Request, res: Response) => {
 export const fishes = async (req: Request, res: Response) => {
 
     res.render('pages/page', {
+        menu: createMenuObject('fish'),
         banner: {
             title: 'Peixes',
             background: 'banner_fish.jpg'
